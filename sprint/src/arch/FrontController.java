be018f2239cf498dad3a2fac3ff0a3c7bd3ced56
Yoannah05 <hyyoannah@gmail.com>
2025-02-18 14:17:17 +0300

    sprint02

--- sprint/src/arch/FrontController.java
+++ sprint/src/arch/FrontController.java
@@ -3,29 +3,52 @@ package arch;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import arch.annotation.Controller;
+import arch.annotation.GET;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 public class FrontController extends HttpServlet {
 
-    private boolean controllersScanned = false;
-    private List<String> controllerList = new ArrayList<>();
+    private Map<String, Mapping> urlMappings = new HashMap<>();  
     private String controllerPackage;
+    private int test = 0;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         controllerPackage = config.getInitParameter("controller-package");
+        scanControllers();
     }
 
+    // Méthode pour scanner les contrôleurs et associer les méthodes GET à des URL
+    private void scanControllers() {
+        try {
+            List<Class<?>> controllerClasses = getClasses(controllerPackage);
+
+            for (Class<?> clazz : controllerClasses) {
+                if (clazz.isAnnotationPresent(Controller.class)) {
+                    for (Method method : clazz.getDeclaredMethods()) {
+                        if (method.isAnnotationPresent(GET.class)) {
+                            GET getAnnotation = method.getAnnotation(GET.class);
+                            String url = getAnnotation.value();
+                            Mapping mapping = new Mapping(clazz.getName(), method.getName());
+                            urlMappings.put(url, mapping); 
+                        }
+                        test++;
+                    }
+                }
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Méthode pour obtenir les classes dans un package donné
     private List<Class<?>> getClasses(String packageName) throws ClassNotFoundException, IOException {
         List<Class<?>> classes = new ArrayList<>();
-
-        // Use ClassLoader to obtain the classes
         ClassLoader classLoader = getClass().getClassLoader();
         String path = packageName.replace('.', '/');
         URL packageUrl = classLoader.getResource(path);
@@ -44,7 +67,6 @@ public class FrontController extends HttpServlet {
         } else {
             throw new IOException("Package not found: " + packageName);
         }
-
         return classes;
     }
 
@@ -58,48 +80,23 @@ public class FrontController extends HttpServlet {
         processRequest(request, response);
     }
 
+    // Méthode pour traiter la requête et afficher le Mapping associé à l'URL
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().println("<html><body>");
-        response.getWriter().println("<h1>List of Scanned Controllers:</h1>");
+        response.getWriter().println("<h1>URL Mapping:</h1>");
 
-        // If controllers haven't been scanned yet, do so
-        if (!controllersScanned) {
-            try {
-                controllerList.clear(); // Clear the previous list
-                List<Class<?>> controllerClasses = getClasses(controllerPackage);
+        String requestUrl = request.getPathInfo();
 
-                for (Class<?> clazz : controllerClasses) {
-                    if (clazz.isAnnotationPresent(Controller.class)) {
-                        String controllerName = clazz.getName();
-                        controllerList.add(controllerName);
-                        System.out.println("Controller found: " + controllerName);
-                    }
-                }
+        Mapping mapping = urlMappings.get(requestUrl); 
 
-                // Mark controllers as scanned to avoid re-scanning
-                controllersScanned = true;
-
-                // Display the list of scanned controllers
-                System.out.println("Scanned controllers:");
-                for (String controller : controllerList) {
-                    System.out.println(controller);
-                }
-            } catch (ClassNotFoundException | IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Display controllers in the response body
-        if (!controllerList.isEmpty()) {
-            response.getWriter().println("<ul>");
-            for (String controller : controllerList) {
-                response.getWriter().println("<li>" + controller + "</li>");
-            }
-            response.getWriter().println("</ul>");
+        if (mapping != null) {
+            response.getWriter().println("<p>URL: " + requestUrl + "</p>");
+            response.getWriter().println("<p>Mapped to: " + mapping + "</p>");
         } else {
-            response.getWriter().println("<p>No controllers found.</p>");
+            response.getWriter().println("<p>No method associated with this URL : </p>" + requestUrl);
         }
+        response.getWriter().println("<p>"+test+"</p>");
 
         response.getWriter().println("</body></html>");
     }
