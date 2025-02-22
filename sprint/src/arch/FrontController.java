b722457186a078498d7369f3ff95c4975f18da86
Yoannah05 <hyyoannah@gmail.com>
2025-02-22 13:57:31 +0300

    sprint07

--- sprint/src/arch/FrontController.java
+++ sprint/src/arch/FrontController.java
@@ -1,6 +1,8 @@
 package arch;
 
+import arch.annotation.FormField;
 import arch.annotation.Param;
+import arch.annotation.RequestParam;
 import arch.exception.FrameworkException;
 import arch.exception.UnknownResultTypeException;
 import arch.exception.UrlMappingNotFoundException;
@@ -15,6 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -27,10 +30,10 @@ public class FrontController extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         String controllerPackage = config.getInitParameter("controller-package");
-        
+
         this.mappingRegistry = new MappingRegistry();
         this.resultHandler = new ResultHandler();
-        
+
         ControllerScanner scanner = new ControllerScanner(mappingRegistry, controllerPackage);
         scanner.scanControllers();
     }
@@ -77,56 +80,91 @@ public class FrontController extends HttpServlet {
         return requestURL;
     }
 
+    // FrontController.java - Méthode invokeHandler modifiée
     private Object invokeHandler(Mapping mapping, HttpServletRequest request) throws Exception {
         try {
-            // Chargement dynamique de la classe
+            // Chargement de la classe
             Class<?> clazz = Class.forName(mapping.getClassName());
             Object instance = clazz.getDeclaredConstructor().newInstance();
-            
-            // Récupération de la méthode avec son nom
-            Method method = clazz.getDeclaredMethod(mapping.getMethodName(), String.class); // Ajustez les types si nécessaire
-            
-            // Récupération des paramètres de la méthode
-            Parameter[] parameters = method.getParameters();
-            Object[] args = new Object[parameters.length];
-            
-            // Traitement des paramètres annotés avec @Param
-            for (int i = 0; i < parameters.length; i++) {
-                Parameter param = parameters[i];
-                Param annotation = param.getAnnotation(Param.class);
-                
-                if (annotation != null) {
-                    // Extraction du nom du paramètre à partir de l'annotation @Param
-                    String paramName = annotation.name();
-                    
-                    // Récupération de la valeur du paramètre dans la requête HTTP
-                    String paramValue = request.getParameter(paramName);
-                    
-                    if (paramValue != null) {
-                        // Conversion du type du paramètre et injection
-                        args[i] = convertParameterValue(paramValue, param.getType());
-                    }
+
+            // Récupération de la méthode avec ses paramètres
+            Method methodToFind = null;
+            for (Method m : clazz.getDeclaredMethods()) {
+                if (m.getName().equals(mapping.getMethodName())) {
+                    methodToFind = m;
+                    break;
                 }
             }
-            
-            // Invocation de la méthode
-            return method.invoke(instance, args);
+
+            if (methodToFind == null) {
+                throw new NoSuchMethodException(
+                        "Méthode " + mapping.getMethodName() + " non trouvée dans " + mapping.getClassName());
+            }
+
+            // Récupération des paramètres de la méthode
+            Parameter[] parameters = methodToFind.getParameters();
+            Object[] args = new Object[parameters.length];
+
+            // Traitement des paramètres
+            for (int i = 0; i < parameters.length; i++) {
+                Parameter param = parameters[i];
+                if (param.isAnnotationPresent(Param.class)) {
+                    // Paramètre simple avec @Param
+                    Param annotation = param.getAnnotation(Param.class);
+                    String paramName = annotation.name();
+                    String paramValue = request.getParameter(paramName);
+                    args[i] = convertParameterValue(paramValue, param.getType());
+                } else if (param.isAnnotationPresent(RequestParam.class)) {
+                    // Objet complexe avec @RequestParam
+                    args[i] = bindRequestParameters(param.getType(), request);
+                } else {
+                    // Si pas d'annotation, utiliser le nom du paramètre directement
+                    String paramName = param.getName();
+                    String paramValue = request.getParameter(paramName);
+                    args[i] = convertParameterValue(paramValue, param.getType());
+                }
+            }
+
+            // Invocation de la méthode avec les arguments
+            return methodToFind.invoke(instance, args);
+
         } catch (NoSuchMethodException e) {
-            // Gérer l'absence de la méthode
             throw new Exception("Méthode introuvable : " + mapping.getMethodName(), e);
         } catch (InvocationTargetException e) {
-            // Gérer les erreurs internes pendant l'exécution de la méthode
             throw new Exception("Erreur lors de l'exécution de la méthode : " + mapping.getMethodName(), e);
         } catch (Exception e) {
-            // Gestion d'autres erreurs générales
             throw new Exception("Erreur lors de l'invocation du handler pour " + mapping.getClassName(), e);
         }
     }
-    
+
+    private Object bindRequestParameters(Class<?> paramType, HttpServletRequest request) throws Exception {
+        Object instance = paramType.getDeclaredConstructor().newInstance();
+
+        for (Field field : paramType.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            String paramName;
+            if (field.isAnnotationPresent(FormField.class)) {
+                FormField annotation = field.getAnnotation(FormField.class);
+                paramName = !annotation.name().isEmpty() ? annotation.name() : field.getName();
+            } else {
+                paramName = field.getName();
+            }
+
+            String paramValue = request.getParameter(paramName);
+            if (paramValue != null && !paramValue.isEmpty()) {
+                Object convertedValue = convertParameterValue(paramValue, field.getType());
+                field.set(instance, convertedValue);
+            }
+        }
+
+        return instance;
+    }
 
     private Object convertParameterValue(String value, Class<?> type) {
-        if (value == null) return null;
-        
+        if (value == null)
+            return null;
+
         if (type == String.class) {
             return value;
         } else if (type == int.class || type == Integer.class) {
@@ -140,7 +178,7 @@ public class FrontController extends HttpServlet {
         } else if (type == float.class || type == Float.class) {
             return Float.parseFloat(value);
         }
-        
+
         throw new UnknownResultTypeException("Unsupported parameter type: " + type.getName());
     }
 
