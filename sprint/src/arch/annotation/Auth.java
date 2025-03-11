a1da9593cb6f5b23ab3305b5a82424147868edab
Yoannah05 <hyyoannah@gmail.com>
2025-03-11 21:45:23 +0300

    sprint16

--- sprint/src/arch/annotation/Auth.java
+++ sprint/src/arch/annotation/Auth.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * If roles are specified, both authentication and appropriate role are required.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Auth {
     /**
      * List of roles that are allowed to access the annotated method.
